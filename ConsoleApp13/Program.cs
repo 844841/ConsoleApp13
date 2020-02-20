c03743865610dede68680a59696a0799f576bf1a
15341a1205 <41323544+15341a1205@users.noreply.github.com>
2020-02-20 14:53:49 +0530

    Initial commit

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            exception e = new exception();
+            e.division(25, 0);
+            Console.ReadKey();
+        }
+    }
+}
